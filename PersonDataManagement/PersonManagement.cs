e5fac4c0c08f2ce79576b1fda493052de8670784
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-05-20 15:51:27 +0530

    [VINAY] Add: Adding Defaults Persons Data in the List

---
+++ PersonDataManagement/PersonManagement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDataManagement
+{
+    public class PersonManagement
+    {
+        List<Person> list = new List<Person>();
+        public void AddPerson()
+        {
+            list.Add(new Person { SSN = 101, Name = "Ajay", Address = "Lucknow", Age = 55 });
+            list.Add(new Person { SSN = 102, Name = "Vijay", Address = "Delhi", Age = 70 });
+            list.Add(new Person { SSN = 103, Name = "Aman", Address = "Basti", Age = 80 });
+            list.Add(new Person { SSN = 104, Name = "Anish", Address = "GorakhPur", Age = 40 });
+            list.Add(new Person { SSN = 105, Name = "Suraj", Address = "Noida", Age = 15 });
+            list.Add(new Person { SSN = 106, Name = "Anurag", Address = "Gaziyabad", Age = 55 });
+        }
+    }
+}
---
+++ PersonDataManagement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using PersonDataManagement;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("               >>>>>>>>>******** >>> WELCOME IN THE LAMDA EXPRESSION <<< ********<<<<<<<<<");
+        PersonManagement person = new PersonManagement();
+        bool check = true;
+        while (check)
+        { 
+        Console.WriteLine(" Adding the Data  in The List 1\n");
+            Console.WriteLine("###############Enter the Above option to Excute the Use Cases################");
+            int option =Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    person.AddPerson();
+                    break;
+                case 0:
+                    break;
+            }
+        }
+       
+        
+
+    }
+}
